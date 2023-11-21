35e32eb5d19b31c5eb6fa662cc2ee12e9f4ed363
KarimKashkoush <karimkashkoush5@gmail.com>
2023-11-21 21:30:58 +0200

    add

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -1,29 +1,25 @@
 // -_- ;oading Page -_-
 let loading = document.getElementById("loading");
-window.onload = _=> {
-    loading.style.display= "none";
-}
-
-
+window.onload = (_) => {
+    loading.style.display = "none";
+};
 // -_- open and close nav -_-
 let navBar = document.getElementById("navBar");
 let openNavBtn = document.getElementById("openNav");
 let closeNavBtn = document.getElementById("closeNav");
-
 openNavBtn.onclick = (_) => {
     navBar.style.left = 0;
 };
 closeNavBtn.onclick = (_) => {
     navBar.style.left = "-100%";
 };
-
 // -_- backGround navbar on scroll -_-
 let header = document.getElementById("header");
 let body = document.body;
-body.onscroll = _=> {
+body.onscroll = (_) => {
     if (window.scrollY > 60) {
-        header.style.backgroundColor = '#1e1e1e'
+        header.style.backgroundColor = "#1e1e1e";
     } else {
-        header.style.backgroundColor = 'transparent'
+        header.style.backgroundColor = "transparent";
     }
-}
+};
