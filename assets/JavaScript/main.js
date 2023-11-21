92ee54f63980f49a341ed8473f334eb8c34abbbf
KarimKashkoush <karimkashkoush5@gmail.com>
2023-11-21 19:10:23 +0200

    add

---
+++ assets/JavaScript/main.js
@@ -0,0 +1,22 @@
+// -_- open and close nav -_-
+let navBar = document.getElementById("navBar");
+let openNavBtn = document.getElementById("openNav");
+let closeNavBtn = document.getElementById("closeNav");
+
+openNavBtn.onclick = (_) => {
+    navBar.style.left = 0;
+};
+closeNavBtn.onclick = (_) => {
+    navBar.style.left = "-100%";
+};
+
+// -_- backGround navbar on scroll -_-
+let header = document.getElementById("header");
+let body = document.body;
+body.onscroll = _=> {
+    if (window.scrollY > 60) {
+        header.style.backgroundColor = '#1e1e1e'
+    } else {
+        header.style.backgroundColor = 'transparent'
+    }
+}
